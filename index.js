3620c0a16b75bbdbb18895f5bcc2d9cbc0ac47ed
jonchurch <me@jonchurch.com>
2018-07-28 00:56:20 +0700

    remove console.log

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ const quotes = require('./quotes')
 
 exports.randomQuote = () => {
 	var item = quotes[Math.floor(Math.random()*quotes.length)];
-	console.log({item})
-
 	return `${item[0]} \n~${item[1]}`
 }
